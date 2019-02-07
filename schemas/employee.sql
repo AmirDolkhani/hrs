7530669ad5f8d0547aad301375adb83dc96eb00a
Amir Dolkhani <amirdolkhani@gmail.com>
2019-02-07 16:29:59 +0100

    Add schemas

---
+++ schemas/employee.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `employee`(
+     `id` CHAR(36) NOT NULL PRIMARY KEY,
+     `name` VARCHAR(30),
+     `lastName` VARCHAR(30),
+     `socialSecurityNumber` VARCHAR(10),
+     `birthday` VARCHAR(10),
+     `salary` DOUBLE
+ )
+
+-- Added Employee table
