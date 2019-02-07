0b5ac9ceb55084c7162df6fae513a656589412a7
Amir Dolkhani <amirdolkhani@gmail.com>
2019-02-07 16:16:57 +0100

    Add finally

--- src/main/java/com/amirdolkhani/hrs/repository/MySQLEmployeeRepository.java
+++ src/main/java/com/amirdolkhani/hrs/repository/MySQLEmployeeRepository.java
@@ -4,41 +4,58 @@ import com.amirdolkhani.hrs.entity.Employee;
 
 import java.sql.*;
 import java.util.UUID;
+
 public class MySQLEmployeeRepository {
 
     public Employee save(Employee employee) {
         UUID uuid = UUID.randomUUID();
         String randomUUIDString = uuid.toString();
 
-        try
-        {
-            String url = "jdbc:mysql://18.202.69.163:3306/amiralidb";
-            String user = "amirali";
-            String password = "password";
+        Connection myConn = null;
+        PreparedStatement myStmt = null;
 
-            Connection myConn = null;
-            PreparedStatement myStmt = null;
+        try {
+            String url = "jdbc:mysql://18.202.69.163:3306/amiralidb";
+             String user = "amirali";
+            String password = "password";
 
             // 1: Get a connection to datsbase
             myConn = DriverManager.getConnection(url, user, password);
             // 2: Create a prepared   statement
             myStmt = myConn.prepareStatement("INSERT into employee VALUES(?, ?, ?, ?, ?, ?)");
             // 3: Set the parameters
-            myStmt.setString(1,randomUUIDString);
-            myStmt.setString(2,employee.getName());
-            myStmt.setString(3,employee.getLastName());
-            myStmt.setString(4,employee.getSocialSecurityNumber());
-            myStmt.setString(5,employee.getBirthday());
-            myStmt.setDouble(6,employee.getSalary());
+            myStmt.setString(1, randomUUIDString);
+            myStmt.setString(2, employee.getName());
+            myStmt.setString(3, employee.getLastName());
+            myStmt.setString(4, employee.getSocialSecurityNumber());
+            myStmt.setString(5, employee.getBirthday());
+            myStmt.setDouble(6, employee.getSalary());
             // 4: Execute SQL query
             myStmt.executeUpdate();
             myStmt.close();
 
-        }
-        catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (myStmt != null) {
+                try {
+                    myStmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (myConn != null) {
+                try {
+                    myConn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return employee;
     }
+    public Employee getEmployeeById(String id){
+        return null;
+    }
+
 }
